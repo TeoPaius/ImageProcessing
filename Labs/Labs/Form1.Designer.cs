28f896d72f62ce44f27f6285d6ca56bd6881027c
Teo <teodor_paius@yahoo.com>
2019-03-01 09:55:28 +0200

    lab1 done

---
+++ Labs/Labs/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Labs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.refferencePb = new System.Windows.Forms.PictureBox();
+            this.moddifiedPb = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.loadTsb = new System.Windows.Forms.ToolStripButton();
+            this.saveTsb = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.refferencePb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moddifiedPb)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // refferencePb
+            // 
+            this.refferencePb.Location = new System.Drawing.Point(27, 96);
+            this.refferencePb.Name = "refferencePb";
+            this.refferencePb.Size = new System.Drawing.Size(332, 342);
+            this.refferencePb.TabIndex = 0;
+            this.refferencePb.TabStop = false;
+            // 
+            // moddifiedPb
+            // 
+            this.moddifiedPb.Location = new System.Drawing.Point(452, 96);
+            this.moddifiedPb.Name = "moddifiedPb";
+            this.moddifiedPb.Size = new System.Drawing.Size(336, 342);
+            this.moddifiedPb.TabIndex = 1;
+            this.moddifiedPb.TabStop = false;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadTsb,
+            this.saveTsb});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // loadTsb
+            // 
+            this.loadTsb.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.loadTsb.Image = ((System.Drawing.Image)(resources.GetObject("loadTsb.Image")));
+            this.loadTsb.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.loadTsb.Name = "loadTsb";
+            this.loadTsb.Size = new System.Drawing.Size(37, 22);
+            this.loadTsb.Text = "Load";
+            this.loadTsb.Click += new System.EventHandler(this.loadTsb_Click);
+            // 
+            // saveTsb
+            // 
+            this.saveTsb.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.saveTsb.Image = ((System.Drawing.Image)(resources.GetObject("saveTsb.Image")));
+            this.saveTsb.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.saveTsb.Name = "saveTsb";
+            this.saveTsb.Size = new System.Drawing.Size(35, 22);
+            this.saveTsb.Text = "Save";
+            this.saveTsb.Click += new System.EventHandler(this.saveTsb_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.moddifiedPb);
+            this.Controls.Add(this.refferencePb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.refferencePb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moddifiedPb)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox refferencePb;
+        private System.Windows.Forms.PictureBox moddifiedPb;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton loadTsb;
+        private System.Windows.Forms.ToolStripButton saveTsb;
+    }
+}
+
---
+++ Labs/Labs/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labs
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap im1;
+        Bitmap im2;
+        float R_param = 0.21f;
+        float B_param = 0.07f;
+        float G_param = 0.72f;
+        int threshold = 200;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            im2 = new Bitmap(moddifiedPb.Size.Width,moddifiedPb.Size.Height);
+        }
+
+        private void loadTsb_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Image";
+                dlg.Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|All files (*.*)|*.*";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    im1 = new Bitmap(dlg.FileName);
+                    refferencePb.Image = new Bitmap(im1, refferencePb.Size);
+                    im1 = (Bitmap)refferencePb.Image;
+                }
+            }
+
+            processImage_intensityThresholding(ref im1, ref im2);
+            moddifiedPb.Image = im2;
+        }
+
+
+
+        private void processImage_intensityThresholding(ref Bitmap original, ref Bitmap newImage)
+        {
+            for(int i = 0; i < original.Size.Height; ++i)
+                for(int j = 0; j < original.Size.Width; ++j)
+                {
+                    Color c = original.GetPixel(j, i);
+                    int intensity = (int)(( R_param * c.R + G_param * c.G + B_param * c.B) / (R_param + B_param + G_param));
+
+                    if (intensity < threshold)
+                        intensity = 0;
+                    else
+                        intensity = 255;
+                    newImage.SetPixel(j, i, Color.FromArgb(intensity, intensity, intensity));  
+                }
+        }
+
+        private void saveTsb_Click_1(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+            saveFileDialog1.Title = "Save an Image File";
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.  
+            if (saveFileDialog1.FileName != "")
+            {
+                // Saves the Image via a FileStream created by the OpenFile method.  
+                System.IO.FileStream fs =
+                   (System.IO.FileStream)saveFileDialog1.OpenFile();
+                // Saves the Image in the appropriate ImageFormat based upon the  
+                // File type selected in the dialog box.  
+                // NOTE that the FilterIndex property is one-based.  
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 1:
+                        im2.Save(fs,
+                           System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+
+                    case 2:
+                        im2.Save(fs,
+                           System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+
+                    case 3:
+                        im2.Save(fs,
+                           System.Drawing.Imaging.ImageFormat.Gif);
+                        break;
+                }
+
+                fs.Close();
+            }
+        }
+    }
+}
